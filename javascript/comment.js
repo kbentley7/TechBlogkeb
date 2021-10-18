d13410af9274557aea34f7a1d3c211386d994734
Kenneth E. Bentley, Sr <kenmo8466@gmail.com>
2021-10-18 11:55:10 -0400

    edited comment.js

--- javascript/comment.js
+++ javascript/comment.js
@@ -1,11 +1,13 @@
 async function commentFormHandler(event) {
     event.preventDefault();
 
+   
     const comment_text = document.querySelector('input[name="comment-body"]').value.trim();
 
     const post_id = window.location.toString().split('/')[
         window.location.toString().split('/').length - 1
     ];
+
 
     if (comment_text) {
         const response = await fetch('/api/comments', {
@@ -18,6 +20,8 @@ async function commentFormHandler(event) {
                 'Content-Type': 'application/json'
             }
         });
+
+
 
         if (response.ok) {
             document.location.reload();
