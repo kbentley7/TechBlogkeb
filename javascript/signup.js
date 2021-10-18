67e90ceb004d8be1401e2cd4ab5c0fb4133342a3
Kenneth E. Bentley, Sr <kenmo8466@gmail.com>
2021-10-18 12:00:38 -0400

    edited signup.js

--- javascript/signup.js
+++ javascript/signup.js
@@ -2,9 +2,11 @@ async function signupFormHandler(event) {
     event.preventDefault();
 
 
+    
     const username = document.querySelector('#username-signup').value.trim();
     const password = document.querySelector('#password-signup').value.trim();
 
+  
     if (username && password) {
         const response = await fetch('/api/users', {
             method: 'POST',
@@ -14,6 +16,8 @@ async function signupFormHandler(event) {
             }),
             headers: { 'Content-Type': 'application/json' }
         });
+
+
         if (response.ok) {
             console.log('success');
 
