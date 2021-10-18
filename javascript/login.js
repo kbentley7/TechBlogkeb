c0a7e81e0cccf534103a1851f9ba3b553b4d68c6
Kenneth E. Bentley, Sr <kenmo8466@gmail.com>
2021-10-18 11:58:45 -0400

    edited login.js

--- javascript/login.js
+++ javascript/login.js
@@ -4,6 +4,8 @@ async function loginFormHandler(event) {
     const username = document.querySelector('#username-login').value.trim();
     const password = document.querySelector('#password-login').value.trim();
   
+   
+    
     if (username && password) {
       const response = await fetch('/api/users/login', {
         method: 'post',
@@ -14,6 +16,8 @@ async function loginFormHandler(event) {
         headers: { 'Content-Type': 'application/json' }
       });
   
+     
+     
       if (response.ok) {
         document.location.replace('/dashboard');
       } else {
